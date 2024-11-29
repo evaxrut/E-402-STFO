a07f29d11f857d520ab9124939dbf613261b521e
evaxrut <136167068+evaxrut@users.noreply.github.com>
2024-11-29 13:37:05 +0000

    union

---
+++ m2/C_DFA_union.py
@@ -0,0 +1,8 @@
+from DFA import DFA
+
+dfa1 = DFA()
+# print("done constructing")
+dfa2 = DFA()
+# print("done constructing")
+
+uniondfa = dfa1.union(dfa2)
--- m2/DFA.py
+++ m2/DFA.py
@@ -1,13 +1,16 @@
+from __future__ import annotations
+from itertools import product
+import sys
+
+sys.setrecursionlimit(100000)
+
+import copy
+
 class DFA:
     def __init__(self) -> None:
         self.states = {}
-        self.num_states = None
-        self.alphabet_size = None
-        self.initial_state = None
-        self.num_final_states = None
-
-        self.final_states = None
-        self.symbols = None
+        self.transitions = list()
+        self.final_states = set()
 
         self.construct()
 
@@ -19,28 +22,29 @@ class DFA:
         self.symbols = input().strip()
 
 
-        self.final_states = list(map(int, input().strip().split()))
+        self.final_states = set(map(int, input().strip().split()))
 
         for i in range(1, self.num_states + 1):
-            self.states[f"state_{i}"] = State(f"state_{i}")
+            self.states[i] = State(i)
 
 
         for i in range(1, self.num_states + 1):
             transitions = input().strip().split()
+            self.transitions.append(transitions)
             for j, symbol in enumerate(self.symbols):
                 destination = int(transitions[j])
-                self.states[f"state_{i}"].connections[symbol] = self.states[f"state_{destination}"]
+                self.states[i].connections[symbol] = self.states[destination]
 
 
         for state in self.final_states:
-            self.states[f"state_{state}"].final = True
+            self.states[state].final = True
 
 
     def read(self):
         strings = int(input())
 
         for i in range(strings):
-            current_state = self.states[f"state_{self.initial_state}"]
+            current_state = self.states[self.initial_state]
 
             transitions = list(input())
 
@@ -58,14 +62,82 @@ class DFA:
                 else:
                     print("reject")
 
-    def complement(self):
-        num_states = None
-        alphabet_size = None
-        initial_state = None
-        num_final_states = None
 
-        final_states = None
-        symbols = None
+    def complement(self):
+        complement_dfa = copy.deepcopy(self)
+        complement_dfa.final_states = [x for x in range(1, self.num_states + 1) if x not in self.final_states]
+
+        print(complement_dfa.num_states, complement_dfa.alphabet_size, complement_dfa.initial_state, len(complement_dfa.final_states))
+        print(complement_dfa.symbols)
+        print(*complement_dfa.final_states)
+        for transition in complement_dfa.transitions:
+            print(*transition)
+
+
+    def union(self, other_dfa: DFA) -> DFA:
+        union_dfa = DFA.__new__(DFA)
+        union_dfa.symbols = self.symbols
+        union_dfa.alphabet_size = self.alphabet_size
+        
+        states_union = set(product(self.states.keys(), other_dfa.states.keys()))
+
+        union_state_names = dict()
+        union_state_idx = 1
+
+        union_dfa.final_states = set()
+        for (s1, s2) in sorted(states_union):
+            union_state_names[s1, s2] = union_state_idx
+            
+            if (s1 in self.final_states) or (s2 in other_dfa.final_states):
+                union_dfa.final_states.add(union_state_idx)
+            
+            union_state_idx += 1 
+
+        union_dfa.num_final_states = len(union_dfa.final_states)
+        union_dfa.initial_state = union_state_names[self.initial_state, other_dfa.initial_state]
+        
+        union_dfa.states = union_state_names
+        union_dfa.num_states = len(union_state_names)
+
+        union_dfa.num_states = len(states_union)
+        union_dfa.num_final_states = len(union_dfa.final_states)
+
+        transitions_union = []
+        for (s1, s2) in sorted(states_union):
+            transition = []
+            for symbol in union_dfa.symbols:
+                next_s1 = self.states[s1].connections[symbol].name
+                next_s2 = other_dfa.states[s2].connections[symbol].name
+                new_state = union_state_names[next_s1, next_s2]
+                transition.append(str(new_state))
+            transitions_union.append(transition)
+
+        union_dfa.transitions = transitions_union
+        union_dfa.print_cause_strings_in_python_dont_concatinate_well()
+
+
+    
+    def print_cause_strings_in_python_dont_concatinate_well(self):
+        print(self.num_states, self.alphabet_size, self.initial_state, len(self.final_states))
+        print(self.symbols)
+        print(*self.final_states)
+        for transition in self.transitions:
+            print(*transition)
+ 
+
+
+    def __str__(self):
+        result = f"{self.num_states} {self.alphabet_size} {self.initial_state} {len(self.final_states)}\n"
+        result += f"{self.symbols}\n"
+        result += " ".join(map(str, self.final_states)) + "\n"
+        for i in range(self.num_states - 1):
+            result += " ".join(self.transitions[i]) + "\n"
+
+        return result
+
+        
+
+            
 
 class State:
     def __init__(self, name):
